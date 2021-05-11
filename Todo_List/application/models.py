7fb37288204a0763f480ec55f30a93d1690c2a38
andrew-barrett <ABarrett@qa.com>
2021-05-11 16:50:13 +0100

    changed completion to default false

--- Todo_List/application/models.py
+++ Todo_List/application/models.py
@@ -3,4 +3,4 @@ from application import db
 class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(30), nullable=False)
-    complete = db.Column(db.Boolean, nullable=False)
+    complete = db.Column(db.Boolean, default=False)
