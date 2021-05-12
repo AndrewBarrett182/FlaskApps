f3402099913bb713d6d63523c192eeea79c99322
andrew-barrett <ABarrett@qa.com>
2021-05-12 16:09:50 +0100

    added new routes and added templates

--- Todo_List/application/__init__.py
+++ Todo_List/application/__init__.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.89.74.237:3306/todo" # Set the connection string to connect to a database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.89.74.237:3306/todo" # Set the connection string to connect to a database
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+
 db = SQLAlchemy(app) # Declare SQLAlchemy object
 
 from application import routes
--- Todo_List/application/routes.py
+++ Todo_List/application/routes.py
@@ -1,14 +1,29 @@
 from application import app, db
 from application.models import Todos
+from flask import render_template
 
 @app.route('/')
-def home():
-    return 'THIS IS A TODO LIST'
-
 @app.route('/todos')
 def todo():
-    all_todo = Todos.query.all()
-    todo_string = "<h1>To do List: </h1>"
-    for todo in all_todo:
-        todo_string += f"Task: {todo.task} <br>Complete: {str(todo.complete)} <br><br>"
-    return todo_string
+    return render_template('index.html', all_todo = Todos.query.all())
+
+@app.route('/add')
+def add():
+    new_todo = Todos(task="New Todo")
+    db.session.add(new_todo)
+    db.session.commit()
+    return "Added new todo to database"
+
+@app.route('/complete/<id>')
+def update(id):
+    to_do = Todos.query.filter_by(id=id).first()
+    to_do.complete = True
+    db.session.commit()
+    return f"Completed todo {id}"
+
+@app.route('/delete/<id>')
+def delete(id):
+    to_do = Todos.query.filter_by(id=id).first()
+    db.session.delete(to_do)
+    db.session.commit()
+    return f"Deleted: {to_do.task} from database."
